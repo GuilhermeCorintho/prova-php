c957c32b0a24622b5f62dfdd382d61fc23f0ecf6
Guilherme Corintho <guilhermecorintho@hotmail.com>
2020-08-02 19:31:40 -0300

    Adding programming-logic.php file with a function that return the sum of all numbers that are multiple of 3 or 5 and lower than 1000

---
+++ programming-logic.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Return the sum of all numbers that are multiple of 3 or 5 and lower than 1000
+ */
+function main () {
+    $sum = 0;
+    for ($i = 999; $i >= 1; $i--) {
+      if ($i % 3 === 0 || $i % 5 === 0) {
+        $sum += $i;
+      }
+    }
+    return $sum;
+}
+
+echo main ();
